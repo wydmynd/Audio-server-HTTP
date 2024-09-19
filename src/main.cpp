daaea95f2841306fe829ad97b66257cdf0038fc8
wydmynd <tomsofer@gmail.com>
2024-09-19 12:53:03 +0300

    small tweaks, removed redundant i2s config

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,9 @@ SemaphoreHandle_t audioMutex = NULL;
 
 
 // I2S configuration
-i2s_config_t i2s_config = {
+i2s_config_t i2s_default_config = {
   .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-  .sample_rate = 11025,//22050,
+  .sample_rate = 22050,
   .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
   .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
   .communication_format = I2S_COMM_FORMAT_STAND_I2S,
@@ -96,20 +96,10 @@ bool readWAVHeader(File &file, WAVHeader &header) {
 }
 
 void configureI2S(const WAVHeader &header) {
-    i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
-        .sample_rate = header.sampleRate,
-        .bits_per_sample = (i2s_bits_per_sample_t)header.bitsPerSample,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 8,
-        .dma_buf_len = 64,
-        .use_apll = false,
-        .tx_desc_auto_clear = true,
-        .fixed_mclk = 0
-    };
-
+    i2s_config_t i2s_config = i2s_default_config; //begin from default configuration
+    i2s_config.sample_rate = header.sampleRate; //change sample rate according to WAV file
+    i2s_config.bits_per_sample = (i2s_bits_per_sample_t)header.bitsPerSample; //change bits per sample according to WAV file
+    
     i2s_driver_uninstall(I2S_NUM_0);
     i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
     i2s_set_pin(I2S_NUM_0, &pin_config);
@@ -191,14 +181,14 @@ void setup() {
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
     Serial.println("Connecting to WiFi...");
+    delay(1000);    
   }
   Serial.println("Connected to WiFi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_driver_install(I2S_NUM_0, &i2s_default_config, 0, NULL);
   i2s_set_pin(I2S_NUM_0, &pin_config);
 
   audioMutex = xSemaphoreCreateMutex();
